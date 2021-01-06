3691bd7203c2c174e95303f1fa220dede3feb428
James Gould <nobleburgundy@gmail.com>
2021-01-06 09:04:21 -0700

    Initial setup and db schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured)
+	 VALUES ("Cheeseburger", false),
+		    ("Hamburger", false),
+		    ("Jucy Lucy", false),
+		    ("Double Cheeseburger", false),
+		    ("Bison Burger", false),
+		    ("Big Mac", false),
+            ("Slider", false),
+            ("Black Bean Burger", false),
+            ("Impossible Burger", false);
