12a671210ba4e7f88d00d802fbddf363170d50c8
James Gould <nobleburgundy@gmail.com>
2021-01-07 22:40:13 -0700

    Added 'devoured' section and excluded devoured from waitlist

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,12 +3,12 @@ USE burger_db;
 INSERT INTO burgers (burger_name, devoured)
 	 VALUES ("Cheeseburger", false),
 		    ("Hamburger", false),
-		    ("Jucy Lucy", false),
+		    ("Jucy Lucy", true),
 		    ("Double Cheeseburger", false),
-		    ("Bison Burger", false),
+		    ("Bison Burger", true),
             ("Elk Burger", false),
 		    ("Big Mac", false),
             ("Slider", false),
             ("Black Bean Burger", false),
-            ("Impossible Burger", false),
+            ("Impossible Burger", true),
             ("Salmon Burger", false);
